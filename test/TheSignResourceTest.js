3bac8e2a2161bacc8d70d9e39e9825b22ca5ef69
okunishinishi <okunishinishi@gmail.com>
2018-01-05 20:39:32 +0900

    Version incremented to 1.0.2

---
+++ test/TheSignResourceTest.js
@@ -0,0 +1,22 @@
+/**
+ * Test for TheSignResource.
+ * Runs with mocha.
+ */
+'use strict'
+
+const TheSignResource = require('../lib/TheSignResource')
+const { ok, equal } = require('assert')
+
+describe('the-sign-resource', () => {
+  before(() => {
+  })
+
+  after(() => {
+  })
+
+  it('Do test', () => {
+
+  })
+})
+
+/* global describe, before, after, it */
